df471ef2121b6b97fd1955d241ca20c6ba15f213
Albarral <albarral@migtron.com>
2018-04-18 13:20:03 +0200

    goon core: adapted to new Brooks controls

--- goon_core/src/goon/core/GoonBus.h
+++ goon_core/src/goon/core/GoonBus.h
@@ -10,9 +10,11 @@
 #include <opencv2/core/core.hpp>
 
 #include "tron/control/brooks/control.h"
+#include "tron/control/brooks/control2.h"
 #include "tron/control/brooks/controlT.h"
 #include "tron/control/brooks/sensor.h"
-#include "tron/control/brooks/sensorT.h"
+#include "tron/control/brooks/sensor2.h"
+#include "tron/control/brooks/sensor3.h"
 
 namespace goon 
 {
@@ -23,45 +25,45 @@ class GoonBus
         // CONTROLS
         // Focus
         tron::Control CO_FOCUS_SHIFT;
-        tron::ControlT<int> CO_FOCUS_MODE;
+        tron::Control2 CO_FOCUS_MODE;
         tron::ControlT<cv::Vec3f> CO_FOCUS_SEARCH_VALUE;
 
         // SENSORS
         // Look
-        tron::SensorT<int> SO_LOOK_BEAT;  // Look module beat
-        tron::SensorT<int> SO_LOOK_OBJECT;  // identified object
+        tron::Sensor2 SO_LOOK_BEAT;  // Look module beat
+        tron::Sensor2 SO_LOOK_OBJECT;  // identified object
         // Focus
-        tron::SensorT<int> SO_FOCUS_BEAT;  // Focus module beat
+        tron::Sensor2 SO_FOCUS_BEAT;  // Focus module beat
         tron::Sensor SO_FOCUS_FOCUSED;  // target focused flag
-        tron::SensorT<int> SO_FOCUS_ROI;  // target focused ROI
+        tron::Sensor2 SO_FOCUS_ROI;  // target focused ROI
         // See
-        tron::SensorT<int> SO_SEE_BEAT;  // See module beat
-        tron::SensorT<float> SO_SEE_FPS;  // frequency of See module
+        tron::Sensor2 SO_SEE_BEAT;  // See module beat
+        tron::Sensor3 SO_SEE_FPS;  // frequency of See module
         // Grab
-        tron::SensorT<int> SO_GRAB_BEAT;  // Grab module beat
+        tron::Sensor2 SO_GRAB_BEAT;  // Grab module beat
         
     public:
         GoonBus();
 
         // CONTROLS        
         tron::Control& getCO_FOCUS_SHIFT() {return CO_FOCUS_SHIFT;};
-        tron::ControlT<int>& getCO_FOCUS_MODE() {return CO_FOCUS_MODE;};
+        tron::Control2& getCO_FOCUS_MODE() {return CO_FOCUS_MODE;};
         tron::ControlT<cv::Vec3f>& getCO_FOCUS_SEARCH_VALUE() {return CO_FOCUS_SEARCH_VALUE;};
 
         
         // SENSORS
         // Look
-        tron::SensorT<int>& getSO_LOOK_BEAT() {return SO_LOOK_BEAT;};  
-        tron::SensorT<int>& getSO_LOOK_OBJECT() {return SO_LOOK_OBJECT;};  
+        tron::Sensor2& getSO_LOOK_BEAT() {return SO_LOOK_BEAT;};  
+        tron::Sensor2& getSO_LOOK_OBJECT() {return SO_LOOK_OBJECT;};  
         // Focus
-        tron::SensorT<int>& getSO_FOCUS_BEAT() {return SO_FOCUS_BEAT;};  
+        tron::Sensor2& getSO_FOCUS_BEAT() {return SO_FOCUS_BEAT;};  
         tron::Sensor& getSO_FOCUS_FOCUSED() {return SO_FOCUS_FOCUSED;};  
-        tron::SensorT<int>& getSO_FOCUS_ROI() {return SO_FOCUS_ROI;};  
+        tron::Sensor2& getSO_FOCUS_ROI() {return SO_FOCUS_ROI;};  
         // See
-        tron::SensorT<int>& getSO_SEE_BEAT() {return SO_SEE_BEAT;};  
-        tron::SensorT<float>& getSO_SEE_FPS() {return SO_SEE_FPS;};
+        tron::Sensor2& getSO_SEE_BEAT() {return SO_SEE_BEAT;};  
+        tron::Sensor3& getSO_SEE_FPS() {return SO_SEE_FPS;};
         // Grab
-        tron::SensorT<int>& getSO_GRAB_BEAT() {return SO_GRAB_BEAT;};  
+        tron::Sensor2& getSO_GRAB_BEAT() {return SO_GRAB_BEAT;};  
 };
 
 }
